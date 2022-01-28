292d9b3f03aba2d030a8fe8832b48cf32ce2d648
ArthurRosin <arthur.rosin@aluno.ufabc.edu.br>
2022-01-28 16:41:09 -0300

    commit atividades

--- TiposPrimitivos/src/ex2/git/TiposPrimitivos.java
+++ TiposPrimitivos/src/ex2/git/TiposPrimitivos.java
@@ -10,7 +10,11 @@ package ex2.git;
 public class TiposPrimitivos {
 
 	public static void main(String[] args) {
+		float nota_1 = 8.5f;
+		float nota_2 = (float)9.5;
 		
+		System.out.printf("A nota 1: %.2f", nota_1);
+		System.out.printf(" e A nota 2: %.2f", nota_2);
 
 	}
 
